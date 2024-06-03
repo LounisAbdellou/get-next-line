c9792f81d509f9aac415b7a7579a826a66c30170
LounisAbdellou <lounisabdellou.dev@gmail.com>
2024-06-03 18:31:22 +0200

    Project Init

---
+++ get_next_line.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:23:41 by labdello          #+#    #+#             */
+/*   Updated: 2024/06/03 19:24:44 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}
+
+#include <stdio.h>
+int main()
+{
+	unsigned long int a = 4294967295;
+	unsigned long int b = 4294967295;
+
+	printf("%d", a * b);
+}
